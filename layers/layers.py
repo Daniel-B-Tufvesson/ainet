Fixed the backwards pass of gradients.

layers/layers.py
@@ -7,8 +7,9 @@ import random as rand
 class Layer(ABC):
 
     def __init__(self):
-        self.x = np.empty(0)  # The input to this layer.
-        self.y = np.empty(0)  # The output of this layer.
+        self.x = np.empty(0)  # The last input to this layer.
+        self.y = np.empty(0)  # The last output of this layer.
+        #self.gradients = np.empty(0)  # The last (non-local) gradient of this layer.
         self.input_dimensions = (0,)  # The dimensionality of the input.
         self.output_dimensions = (0,)  # The dimensionality of the output.
 
@@ -21,19 +22,47 @@ class Layer(ABC):
         self.output_dimensions = input_dimensions
 
     def forward_pass(self, x: np.ndarray) -> np.ndarray:
+        """
+        Compute the forward pass.
+        :param x: a matrix with shape (input_dimensions) for unbatched, or (n, input_dimensions) for batched with
+        a batch size of n.
+        :return:
+        """
 
-        if self.input_dimensions != x.shape:
-            raise ValueError(f'Input array does not have correct dimensions. Expected: '
-                             f'{self.input_dimensions}, got: {x.shape}')
+        # Check if this is a batched input.
+        if len(x.shape) == len(self.input_dimensions) + 1:
+
+            if self.input_dimensions != x.shape[1:]:
+                raise ValueError(f'Input array does not have correct dimensions. Expected: '
+                                 f'{self.input_dimensions}, got: {x.shape[0:]}')
+
+            # Make the row-vectors into col-vectors.
+            #x = x.transpose()
+
+        # Check unbatched input.
+        else:
+            if self.input_dimensions != x.shape:
+                raise ValueError(f'Input array does not have correct dimensions. Expected: '
+                                 f'{self.input_dimensions}, got: {x.shape}')
+
+            # Promote vector to matrix.
+            #x = x[:, np.newaxis]
+
+
+            #x = x.transpose()  # We want it to be a row vector.
+            #print(x.shape, ", pre-transpose: ", x.transpose().shape)
 
         y = self._compute_forward(x)
         self.x = x
         self.y = y
         return y
 
+    @abstractmethod
     def backward_pass(self, gradients: np.ndarray) -> np.ndarray:
-        local_gradients = self._compute_local_gradients(gradients)
-        return local_gradients * gradients
+        pass
+        #local_gradients = self._compute_local_gradients(gradients)
+        #self.gradients = local_gradients * gradients  # Elementwise mult. correct approach?
+        #return self.gradients
 
     @abstractmethod
     def _compute_forward(self, x: np.ndarray) -> np.ndarray:
@@ -45,11 +74,9 @@ class Layer(ABC):
         pass
 
     @abstractmethod
-    def _compute_local_gradients(self, gradients: np.ndarray) -> np.ndarray:
-        """
-        Compute the local gradients with respect to the input.
-        :param gradients: the upstream gradients.
-        :return: the local gradients.
+    def update_parameters(self, learning_rate):
+        """Update the parameters of this layer using its gradients.
+        :param learning_rate:
         """
         pass
 
@@ -65,6 +92,7 @@ class FullyConnected(Layer):
         super().__init__()
         self.units = units  # The number of units in this layer.
         self.weights = np.zeros(0)  # The weight matrix.
+        self.weight_gradients = np.zeros(0)  # The gradients for each weight.
 
     def compile_layer(self, input_dimensions: tuple[int, ...]):
 
@@ -75,19 +103,23 @@ class FullyConnected(Layer):
         super().compile_layer(input_dimensions)
 
         # Create the weight matrix.
-        self.weights = np.zeros(shape=(self.units, input_dimensions[0]))
+        self.weights = np.zeros(shape=(input_dimensions[0], self.units))
         self.rand_init_weights()
 
         # The output is one dimensional with an element for each unit.
         self.output_dimensions = (self.units, )
 
     def _compute_forward(self, x: np.ndarray) -> np.ndarray:
-        print(x.shape)
-        # Matrix multiplication.
-        return self.weights @ x
+        return x @ self.weights
 
-    def _compute_local_gradients(self, gradients: np.ndarray) -> np.ndarray:
-        pass
+    def backward_pass(self, gradients: np.ndarray) -> np.ndarray:
+        self.weight_gradients = self.x.transpose() @ gradients
+        x_gradients = gradients @ self.weights.transpose()
+        return x_gradients
+
+    def update_parameters(self, learning_rate):
+        # SGD optimization.
+        self.weights -= self.weight_gradients * learning_rate
 
     def rand_init_weights(self):
         """
@@ -97,7 +129,11 @@ class FullyConnected(Layer):
 
         for i in range(self.weights.shape[0]):
             for j in range(self.weights.shape[1]):
-                self.weights[i, j] = rand.random() * 2 - 1
+                self.weights[i, j] = (rand.random() * 2 - 1) * 0.0001
+
+        #print('rand weights: ', self.weights)
+
+
 
 
 class ReLU(Layer):
@@ -105,6 +141,16 @@ class ReLU(Layer):
     def _compute_forward(self, x: np.ndarray) -> np.ndarray:
         return np.maximum(x, 0)
 
-    def _compute_local_gradients(self, gradients: np.ndarray) -> np.ndarray:
-        return np.ndarray([1 if x_i > 0 else 0 for x_i in self.x])
+    def backward_pass(self, gradients: np.ndarray) -> np.ndarray:
+
+        #relu_gradient = np.vectorize(lambda x: 1 if x > 0 else 0)
+
+        #return relu_gradient(gradients)
+        return gradients > 0
+
+        #return np.ndarray([1 if x_i > 0 else 0 for x_i in self.x])
+
+    def update_parameters(self, learning_rate):
+        pass
+
 

layers/model.py
@@ -66,7 +66,7 @@ class Sequence(BaseModel):
 
         return transforms
 
-    def _compute_local_gradients(self, gradients: np.ndarray) -> np.ndarray:
+    def backward_pass(self, gradients: np.ndarray) -> np.ndarray:
         # Back-propagate through the layers.
         for layer in reversed(self.layers):
             gradients = layer.backward_pass(gradients)
@@ -76,11 +76,55 @@ class Sequence(BaseModel):
     def predict(self, x: np.ndarray) -> np.ndarray:
         return self.forward_pass(x)
 
-    def fit(self, x: np.ndarray, y: np.ndarray):
-        print('fit not implemented')
+    def fit(self, x: np.ndarray, y: np.ndarray, epochs=1, learning_rate=0.001):
+
+        y = y[:, np.newaxis]
+
+        for epoch in range(epochs):
+            prediction = self.forward_pass(x)
+            loss, error = self.compute_loss(prediction, y)
+            loss_gradients = self.compute_loss_gradients(error)
+
+            #print('loss', loss, 'error', error.shape, 'loss_gradients', loss_gradients.shape)
+
+            print(f'epoch: {epoch+1}/{epochs}.  loss: {loss}')
+            #print('loss gradients: ', loss_gradients.shape)
+
+            # Backpropagate the loss gradients.
+            self.backward_pass(loss_gradients)
+
+            # Update the weights.
+            self.update_parameters(learning_rate)
+
+
+
+    def compute_loss(self, prediction_y: np.ndarray, target_y: np.ndarray) -> tuple[float, np.ndarray]:
+        """
+        Compute the loss and error.
+        :param prediction_y:
+        :param target_y:
+        :return:
+        """
+        #print('prediction: ', prediction_y.shape, ', target: ', target_y.shape)
+        #error = (target_y - prediction_y.transpose()).transpose() # Kinda weird that we have to transpose.
+        error = target_y - prediction_y
+        #print('error: ', error.shape)
+        loss = np.mean(error ** 2)
+        return loss, error
+
+
+    def compute_loss_gradients(self, error: np.ndarray) -> np.ndarray:
+        n = error.shape[0]
+        return 2 * error / n
+
+    def update_parameters(self, learning_rate):
+        for layer in self.layers:
+            layer.update_parameters(learning_rate)
 
     def evaluate(self, x: np.ndarray, y: np.ndarray):
         print('evaluate not implemented.')
+
+
 
 
 def test_sequence_model():
@@ -117,9 +161,12 @@ def test_train_seq_model():
     val_data_x = np.array([new_sample_x() for _ in range(100)])
     val_data_y = np.array([math.prod(x) for x in val_data_x])
 
+    prediction = model.predict(train_data_x)
+    #print(prediction)
+
     # Train the model.
     print('train model...')
-    model.fit(train_data_x, train_data_y)
+    model.fit(train_data_x, train_data_y, epochs=100)
     print('training complete')
 
 